d7f5cf9686883ea13e0bafa890c0a157d7e692d1
jeffvtang <jeff.tang@gmail.com>
2018-05-30 21:11:42 +0000

    updated seed file with hardcoded times

--- db/seeds/events.js
+++ db/seeds/events.js
@@ -1,11 +1,31 @@
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
+
+  function deleteEvents() {
     return knex('events').del()
-      .then(function () {
-        return Promise.all([
-          knex('events').insert({id: '111', name: 'Code event name', description: 'Lighthouse coding', creator_name: 'Alice', creator_email: 'a@gmail.com'}),
-          knex('events').insert({id: '222', name: 'BBQ event', description: 'Lighthouse BBQ', creator_name: 'Chris', creator_email: 'chris@gmail.com'}),
-          knex('events').insert({id: '333', name: 'networking event', description: 'Lighthouse networking', creator_name: 'Peter', creator_email: 'peter@gmail.com'}),
-        ]);
-      });
-  };
-  
+  }
+
+  function deleteTimes() {
+    return knex('times').del()
+  }
+
+  function insertEvents() {
+    return knex('events').insert([
+      { id: '111', name: 'Code event name', description: 'Lighthouse coding', creator_name: 'Alice', creator_email: 'a@gmail.com' },
+      { id: '222', name: 'BBQ event', description: 'Lighthouse BBQ', creator_name: 'Chris', creator_email: 'chris@gmail.com' },
+      { id: '333', name: 'networking event', description: 'Lighthouse networking', creator_name: 'Peter', creator_email: 'peter@gmail.com' }
+    ])
+  }
+
+  function insertTimes() {
+    return knex('times').insert([
+      { event_id: '111', start_time: '19800101', end_time: '19800102' },
+      { event_id: '222', start_time: '19801001', end_time: '19801002' },
+      { event_id: '333', start_time: '19990101', end_time: '19990102' },
+    ])
+  }
+
+  return deleteTimes()
+    .then(deleteEvents)
+    .then(insertEvents)
+    .then(insertTimes)
+}
