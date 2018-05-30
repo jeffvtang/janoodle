babed51d7589bc8e72afaaed73e5488a95632b65
abeprincec <abeprincec@gmail.com>
2018-05-30 20:46:39 +0000

    hardcoded events seed

--- db/seeds/events.js
+++ db/seeds/events.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
     return knex('events').del()
       .then(function () {
         return Promise.all([
-          knex('events').insert({id: 1, name: 'Code event name', description: 'Lighthouse coding', creator_name: 'Alice', creator_email: 'a@gmail.com'}),
-          knex('events').insert({id: 2, name: 'BBQ event', description: 'Lighthouse BBQ', creator_name: 'Chris', creator_email: 'chris@gmail.com'}),
-          knex('events').insert({id: 3, name: 'networking event', description: 'Lighthouse networking', creator_name: 'Peter', creator_email: 'peter@gmail.com'}),
+          knex('events').insert({id: '111', name: 'Code event name', description: 'Lighthouse coding', creator_name: 'Alice', creator_email: 'a@gmail.com'}),
+          knex('events').insert({id: '222', name: 'BBQ event', description: 'Lighthouse BBQ', creator_name: 'Chris', creator_email: 'chris@gmail.com'}),
+          knex('events').insert({id: '333', name: 'networking event', description: 'Lighthouse networking', creator_name: 'Peter', creator_email: 'peter@gmail.com'}),
         ]);
       });
   };
