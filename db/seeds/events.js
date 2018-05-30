11e6f37cadd11cbbc3b884bd43adb4cbb6bfdc15
abeprincec <abeprincec@gmail.com>
2018-05-30 21:16:17 +0000

    added attendees data to events.js

--- db/seeds/events.js
+++ db/seeds/events.js
@@ -1,11 +1,29 @@
 exports.seed = function(knex, Promise) {
-    return knex('events').del()
-      .then(function () {
-        return Promise.all([
-          knex('events').insert({id: '111', name: 'Code event name', description: 'Lighthouse coding', creator_name: 'Alice', creator_email: 'a@gmail.com'}),
-          knex('events').insert({id: '222', name: 'BBQ event', description: 'Lighthouse BBQ', creator_name: 'Chris', creator_email: 'chris@gmail.com'}),
-          knex('events').insert({id: '333', name: 'networking event', description: 'Lighthouse networking', creator_name: 'Peter', creator_email: 'peter@gmail.com'}),
-        ]);
-      });
-  };
-  
+  function deleteActors() {
+    return knex('attendes').del();
+  }
+
+  function deleteMovies() {
+    return knex('events').del();
+  }
+
+  function insertAttendes() {
+    return knex('attendes')
+      .insert([
+        {
+          id: 1,
+          name: 'Joe',
+          email: 'joe@gmail.com',
+          event_id: '111',
+        },
+        {
+          id: 2,
+          name: 'Jessia',
+          email: 'jessica@gmail.com',
+          event_id: '222',
+        },
+        { name: 'Mad Max: Fury Road', year: '2016' },
+      ])
+      .returning('*');
+  }
+};
