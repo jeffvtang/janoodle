13c41a0226580a6f19642c58103b7257b0ffaa26
abeprincec <abeprincec@gmail.com>
2018-05-31 16:17:14 +0000

    fixed poll route

--- routes/makepolladmin.js
+++ routes/makepolladmin.js
@@ -3,9 +3,6 @@ const makePollAdmin = express.Router();
 
 module.exports = function() {
   makePollAdmin.get('/', (req, res) => {
-        
-
-
     if (req.session.eventTitle) {
       const templateVars = { eventTitle: req.session.eventTitle };
 
@@ -18,41 +15,18 @@ module.exports = function() {
   });
 
   makePollAdmin.post('/create', (req, res) => {
-        if (!req.body.eventTitle) {
-            res.sendStatus(400)
-        }
+    if (!req.body.eventTitle) {
+      res.sendStatus(400);
+    }
 
     req.session.eventTitle = req.body.eventTitle;
     const templateVars = { eventTitle: req.session.eventTitle };
     res.render('create', templateVars);
   });
 
-  const express = require('express');
-const makePollAdmin = express.Router();
-
-module.exports = function() {
-  makePollAdmin.get('/', (req, res) => {
-        
-
-
-    if (req.session.eventTitle) {
-      const templateVars = { eventTitle: req.session.eventTitle };
-
-      return res.render('index.ejs', templateVars);
-    } else {
-      return res.render('index.ejs');
-    }
-    console.log(req.session);
-    res.render('index.ejs');
-  });
-
   makePollAdmin.post('/poll', (req, res) => {
-      res.render("poll.ejs")
+    res.render('poll.ejs');
   });
-
-  return makePollAdmin;
-};
-
 
   return makePollAdmin;
 };
