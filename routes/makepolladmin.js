7685509c420cd7125aed06cfccca97ea2d773f88
abeprincec <abeprincec@gmail.com>
2018-05-31 20:10:22 +0000

    insert event and times into db

--- routes/makepolladmin.js
+++ routes/makepolladmin.js
@@ -43,29 +43,41 @@ module.exports = function(knex) {
     res.render('create', templateVars);
   });
 
-  makePollAdmin.get('/poll', (req, res) => {
-    res.render('poll.ejs');
-  });
-
   makePollAdmin.post('/poll', (req, res) => {
-    const urlId = crypto.randomBytes(20).toString('hex');
+    const urlId = crypto.randomBytes(10).toString('hex');
 
     const eventName = req.body.eventTitle;
-    const eventDate = req.body.eventDate;
+    const eventStartDate = req.body.eventStartDate;
+    const eventEndDate = req.body.eventEndDate;
     const eventDescription = req.body.eventDescription;
     const eventCreatorName = req.body.eventCreatorEmail;
     const eventCreatorEmail = req.body.eventCreatorEmail;
 
-    console.log(eventDate, eventDescription, eventCreatorEmail);
-    knex('events').insert({
-      id: urlId,
-      name: eventName,
-      description: eventDescription,
-      creator_name: eventCreatorName,
-      creator_email: eventCreatorEmail,
-    }).then(console.log("*edit*"))
+    console.log(eventDescription, eventCreatorEmail);
+    knex('events')
+      .insert({
+        id: urlId,
+        name: eventName,
+        description: eventDescription,
+        creator_name: eventCreatorName,
+        creator_email: eventCreatorEmail,
+      })
+      .returning('id')
+      .then(function(response) {
+        return knex('times').insert({
+          event_id: response[0],
+          start_time: eventStartDate,
+          end_time: eventEndDate,
+        });
+        console.log(response);
+      });
 
-    return res.render('poll.ejs');
+    //res.render('poll.ejs')
+    res.redirect(`/poll/${urlId}`);
+  });
+
+  makePollAdmin.get('/poll/:id', (req, res) => {
+    res.render('poll.ejs');
   });
 
   return makePollAdmin;
