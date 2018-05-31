71894ba8b168d432c6f8582dab286a6224444b1a
abeprincec <abeprincec@gmail.com>
2018-05-31 18:34:25 +0000

    get event info and insert into database

--- routes/makepolladmin.js
+++ routes/makepolladmin.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const makePollAdmin = express.Router();
+var crypto = require('crypto');
 
-
-module.exports = function (knex) {
+module.exports = function(knex) {
   makePollAdmin.get('/', (req, res) => {
-
     // DataHelpers.getAllEvents()
     //   .then(function (result) {
     //     console.log(result)
     //   })
     //   .catch(function (err) {
     //     console.log(err)
-    //   });  
+    //   });
 
-    knex.select()
+    knex
+      .select()
       .from('events')
-      .then(function (result) {
-        console.log(result)
-        knex.destroy()
+      .then(function(result) {
+        console.log(result);
       })
-      .catch(function (err) {
-        console.log(err)
-        knex.destroy()
-      })
+      .catch(function(err) {
+        console.log(err);
+      });
 
     if (req.session.eventTitle) {
       const templateVars = { eventTitle: req.session.eventTitle };
@@ -47,6 +45,27 @@ module.exports = function (knex) {
 
   makePollAdmin.get('/poll', (req, res) => {
     res.render('poll.ejs');
+  });
+
+  makePollAdmin.post('/poll', (req, res) => {
+    const urlId = crypto.randomBytes(20).toString('hex');
+
+    const eventName = req.body.eventTitle;
+    const eventDate = req.body.eventDate;
+    const eventDescription = req.body.eventDescription;
+    const eventCreatorName = req.body.eventCreatorEmail;
+    const eventCreatorEmail = req.body.eventCreatorEmail;
+
+    console.log(eventDate, eventDescription, eventCreatorEmail);
+    knex('events').insert({
+      id: urlId,
+      name: eventName,
+      description: eventDescription,
+      creator_name: eventCreatorName,
+      creator_email: eventCreatorEmail,
+    }).then(console.log("*edit*"))
+
+    return res.render('poll.ejs');
   });
 
   return makePollAdmin;
