5b8808a56feab04965312ef887e12d48e76642e7
jeffvtang <jeff.tang@gmail.com>
2018-05-31 20:23:11 +0000

    added query for relevant info

--- routes/makepolladmin.js
+++ routes/makepolladmin.js
@@ -21,16 +21,30 @@ module.exports = function (knex) {
 // join attendees on (events.id = attendees.event_id)
 // where events.id = '111a';
 
-    knex.select('attendees.name')
-    .from('events')
-    .join('attendees', 'events.id', '=', 'attendees.event_id')
-    .where('events.id', '111a')
+    // knex.select('attendees.name', 'attendees.id AS attendees_id')
+    // .from('events')
+    // .join('attendees', 'events.id', '=', 'attendees.event_id')
+    // .where('events.id', '111a')
+
+    // SELECT attendees.id AS a_id, attendees.name AS a_name, times.id AS timeslot, is_available AS avail
+    // FROM availabilities
+    // JOIN attendees ON (attendees.id = availabilities.attendee_id)
+    // JOIN events ON (events.id = attendees.event_id)
+    // JOIN times ON (times.id = availabilities.time_id)
+    // WHERE (events.id = '111a');
+
+    knex.select('attendees.id AS a_id', 'attendees.name AS a_name', 'times.id AS timeslot', 'is_available AS avail')
+    .from('availabilities')
+    .join('attendees', 'attendees.id', '=', 'availabilities.attendee_id')
+    .join('events', 'events.id', '=', 'attendees.event_id')
+    .join('times', 'times.id', '=', 'availabilities.time_id')
+    .where('event.id', '111a')
       .then(function (result) {
-        attendeeNames = result
+        results = result
         const templateVars = {
-          attendeeNames: attendeeNames
+          results: results
         }
-        console.log (attendeeNames)
+        console.log (results)
 
         return res.render('index.ejs', templateVars);
       })
