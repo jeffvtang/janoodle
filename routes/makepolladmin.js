3b8578271c2482260f0c5d8575b8f9f54907dc57
jeffvtang <jeff.tang@gmail.com>
2018-06-01 02:40:23 +0000

    added additional seed data, added error getting for get poll/:id

--- routes/makepolladmin.js
+++ routes/makepolladmin.js
@@ -75,38 +75,42 @@ module.exports = function (knex) {
       .join('times', 'times.id', '=', 'availabilities.time_id')
       .where('events.id', event_url)
       .then(function (query1) {
-        // results = result
-        var results = groupByID(query1, 'id');
-        knex.select('times.id AS id', knex.raw(`sum(case when is_available = 't' then 1 else 0 end) as count`), 'start_time AS start', 'end_time AS end')
-          .from('availabilities')
-          .join('times', 'time_id', '=', 'times.id')
-          .where('event_id', event_url)
-          .groupBy('times.id')
-          .orderBy('times.id')
-          .then(function (query2) {
-            var results2 = query2
-            knex.select()
-              .from('events')
-              .where('id', event_url)
-              .then(function (query3) {
-                const templateVars = {
-                  results: results,
-                  results2: results2,
-                  results3: query3
-                }
-                return res.render('poll.ejs', templateVars);
-              })
-          })
+        // console.log(query1.length)
+        if (query1 == []) {
+          console.log('NOT WORKING')
+        } else {
+          var results = groupByID(query1, 'id');
+          // console.log(results)
+          knex.select('times.id AS id', knex.raw(`sum(case when is_available = 't' then 1 else 0 end) as count`), 'start_time AS start', 'end_time AS end')
+            .from('availabilities')
+            .join('times', 'time_id', '=', 'times.id')
+            .where('event_id', event_url)
+            .groupBy('times.id')
+            .orderBy('times.id')
+            .then(function (query2) {
+              console.log (query2.length)
+              if (query2.length === 0){ // a functional doodle should always have times available, otherwise return a bad error
+                return res.sendStatus(400) 
+              }
+              var results2 = query2
+              knex.select()
+                .from('events')
+                .where('id', event_url)
+                .then(function (query3) {
+                  const templateVars = {
+                    results: results,
+                    results2: results2,
+                    results3: query3
+                  }
+                  return res.render('poll.ejs', templateVars);
+                })
+            }
+          )
+        }
       })
       .catch(function (err) {
         console.log(err)
       })
-      .catch(function (err) {
-        console.log(err);
-      });
-
-
   });
-
   return makePollAdmin;
 };
